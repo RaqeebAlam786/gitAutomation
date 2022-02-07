3d012907a219b62531b52189324c40de9e50af1c
raqeebalam1 <97635811+raqeebalam1@users.noreply.github.com>
2022-02-07 17:14:07 +0500

    Update chaiDepartment.py

--- resources/page_objects/chaiDepartment.py
+++ resources/page_objects/chaiDepartment.py
@@ -17,7 +17,7 @@ class CACD(BasePage):
         self.click(ChaiAndCoffee.submit_zip)
 
     def click_ChaiAndCoffee(self):
-        element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/div[9]/div/div/div/div/a[14]/img')
+        element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/div[9]/div/div/div/div/a[15]/img')
         self.driver.execute_script("arguments[0].click();", element)
 
     def click_weekly(self):
