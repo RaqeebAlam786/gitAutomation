a91d602b800857dbc4c4a685e4acad2fa113c624
RaqeebAlam786 <81440719+RaqeebAlam786@users.noreply.github.com>
2022-02-11 16:45:47 +0500

    Update rotikit.py

--- resources/page_objects/rotikit.py
+++ resources/page_objects/rotikit.py
@@ -44,7 +44,7 @@ class RotiBox(BasePage):
         self.driver.execute_script("arguments[0].click();", element)
 
     def click_Once(self):
-        element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/section[1]/div/div/div[2]/div[2]/ul/li[1]')
+        element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/section[1]/div/div/div[2]/div[2]/ul/li[1]/a')
         self.driver.execute_script("arguments[0].click();", element)
 
     def click_WholeWheat(self):
