9f7598a92677fb6681d9f70a6dc9c86ea6d4f422
RaqeebAlam786 <81440719+RaqeebAlam786@users.noreply.github.com>
2022-06-28 03:25:23 +0500

    Update bbq.py

--- resources/page_objects/bbq.py
+++ resources/page_objects/bbq.py
@@ -22,7 +22,7 @@ class BBQKIT(BasePage):
 
     def click_MealKit(self):
         element = self.driver.find_element_by_css_selector(
-            '#searchhide > div.grocerySpecialSlider.clsFoodSpl.clslowspace > div > div > div > div > a:nth-child(14) > img')
+            '#searchhide > div.grocerySpecialSlider.clsFoodSpl.clslowspace > div > div > div > div > a:nth-child(15) > img')
         self.driver.execute_script("arguments[0].click();", element)
 
     def click_bbqKit(self):
