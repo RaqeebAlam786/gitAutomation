05ff3dc0508498fb00f7fd7499f230eeba414397
RaqeebAlam786 <81440719+RaqeebAlam786@users.noreply.github.com>
2022-06-28 14:41:06 +0500

    Update chaiDepartment.py

--- resources/page_objects/chaiDepartment.py
+++ resources/page_objects/chaiDepartment.py
@@ -17,7 +17,7 @@ class CACD(BasePage):
         self.click(ChaiAndCoffee.submit_zip)
 
     def click_ChaiAndCoffee(self):
-        element = self.driver.find_element_by_css_selector('#searchhide > div.grocerySpecialSlider.clsFoodSpl.clslowspace > div > div > div > div > a:nth-child(16) > img')
+        element = self.driver.find_element_by_css_selector('#searchhide > div.grocerySpecialSlider.clsFoodSpl.clslowspace > div > div > div > div > a:nth-child(17) > img')
         self.driver.execute_script("arguments[0].click();", element)
 
     def click_weekly(self):
